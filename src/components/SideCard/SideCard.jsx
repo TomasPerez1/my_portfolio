d2febd37c8ade8d5fe8437edd56960e368b72358
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-02-28 16:00:32 -0300

    styling sidecard

--- src/components/SideCard/SideCard.jsx
+++ src/components/SideCard/SideCard.jsx
@@ -1,44 +1,37 @@
 import "./SideCard.css"
 import Avatar from '@mui/material/Avatar';
 import profile from "../../assets/profile.jpeg"
-import EmailIcon from '@mui/icons-material/Email';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import ContactPageOutlinedIcon from '@mui/icons-material/ContactPageOutlined';
+
 export default function SideCard() {
   return (
     <div className="container">
       <Avatar 
         alt="T" 
         src={profile}
+        sizes="10 200"
+        sx={{
+          marginBottom: "10px",
+          width: "80%",
+          height: "80%",
+        }}
       />
       <div className="item">
-        <EmailIcon
-        sx={{
-          backgroundColor: "brown"
-        }}
-        />
-        <p className="text">tomas@gmail.com</p>
-      </div>
-      <div className="item">
         <LinkedInIcon
-        sx={{
-          backgroundColor: "green"
-        }}
         />
-        <p className="text">ggg</p>
+        <p className="text">Linkedin</p>
       </div>
       <div className="item">
         <GitHubIcon
-        sx={{
-          backgroundColor: "green"
-        }}
         />
-        <p className="text">ggg</p>
+        <p className="text">Git Hub</p>
       </div>
       <div className="item">
         <ContactPageOutlinedIcon
         />
+        <p className="text">CV resume</p>
       </div>
     </div>
   )
