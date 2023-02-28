04a7e3e27a5cb143be49df7fb7b799c1b0af397d
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-02-27 22:31:08 -0300

    try icons

--- src/components/SideCard/SideCard.jsx
+++ src/components/SideCard/SideCard.jsx
@@ -2,17 +2,43 @@ import "./SideCard.css"
 import Avatar from '@mui/material/Avatar';
 import profile from "../../assets/profile.jpeg"
 import EmailIcon from '@mui/icons-material/Email';
-
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import ContactPageOutlinedIcon from '@mui/icons-material/ContactPageOutlined';
 export default function SideCard() {
   return (
     <div className="container">
       <Avatar 
         alt="T" 
-        src={profile} 
+        src={profile}
       />
-      <div>
-        <EmailIcon/>
-        tomas@gmail.com
+      <div className="item">
+        <EmailIcon
+        sx={{
+          backgroundColor: "brown"
+        }}
+        />
+        <p className="text">tomas@gmail.com</p>
+      </div>
+      <div className="item">
+        <LinkedInIcon
+        sx={{
+          backgroundColor: "green"
+        }}
+        />
+        <p className="text">ggg</p>
+      </div>
+      <div className="item">
+        <GitHubIcon
+        sx={{
+          backgroundColor: "green"
+        }}
+        />
+        <p className="text">ggg</p>
+      </div>
+      <div className="item">
+        <ContactPageOutlinedIcon
+        />
       </div>
     </div>
   )
